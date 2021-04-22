491219593f84d6055a0e91a35e2ef2648b245a93
Richard Cho <richardcho100@gmail.com>
2021-04-22 18:08:32 -0400

    Add useStae to pass down hangouts to HangoutList

--- src/features/hangouts/hangoutDashboard/HangoutDashboard.jsx
+++ src/features/hangouts/hangoutDashboard/HangoutDashboard.jsx
@@ -1,15 +1,17 @@
 /* eslint-disable react/prop-types */
-import React from 'react';
+import React, { useState } from 'react';
 import { Grid } from 'semantic-ui-react';
 import HangoutForm from '../hangoutForm/HangoutForm';
 import HangoutList from './HangoutList';
 import { sampleData } from '../../../app/api/sampleData';
 
 export default function HangoutDashboard({ formDisplayed, setFormDisplayed }) {
+  const [hangouts, setHangouts] = useState(sampleData);
+
   return (
     <Grid>
       <Grid.Column width={10}>
-        <HangoutList hangouts={sampleData} />
+        <HangoutList hangouts={hangouts} />
       </Grid.Column>
       <Grid.Column width={6}>
         {formDisplayed
