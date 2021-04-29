ca33fe5dcec704cc386be867b4ea45d004307c76
Richard Cho <richardcho100@gmail.com>
2021-04-28 23:37:57 -0400

    Build Homepage w/out navbar & go to /hangouts w/btn

--- src/app/layout/App.jsx
+++ src/app/layout/App.jsx
@@ -24,13 +24,23 @@ export default function App() {
 
   return (
     <>
-      <Navbar handleCreateFormDisplayed={handleCreateFormDisplayed} />
-      <Container className="main">
-        <Route exact path="/" component={Homepage} />
-        <Route exact path="/hangouts" component={HangoutDashboard} />
-        <Route path="/hangouts/:id" component={HangoutDetailedPage} />
-        <Route path="/createHangout" component={HangoutForm} />
-      </Container>
+      <Route exact path="/" component={Homepage} />
+      {/* Using regex - any route with / and something else will render the following */}
+      {/* Using this to remove navbar from homepage */}
+      <Route
+        path="/(.+)"
+        render={() => (
+          <>
+            <Navbar handleCreateFormDisplayed={handleCreateFormDisplayed} />
+            <Container className="main">
+              <Route exact path="/hangouts" component={HangoutDashboard} />
+              <Route path="/hangouts/:id" component={HangoutDetailedPage} />
+              <Route path="/createHangout" component={HangoutForm} />
+            </Container>
+          </>
+        )}
+      />
+
     </>
   );
 }
--- src/features/home/Homepage.jsx
+++ src/features/home/Homepage.jsx
@@ -1,7 +1,21 @@
+/* eslint-disable react/prop-types */
 import React from 'react';
+import {
+  Button, Container, Header, Icon, Segment,
+} from 'semantic-ui-react';
 
-export default function Homepage() {
+export default function Homepage({ history }) {
   return (
-    <h1>Homepage</h1>
+    <Segment className="homepage" textAlign="center" vertical inverted>
+      <Container>
+        <Header as="h1" inverted>
+          Hangout
+        </Header>
+        <Button onClick={() => history.push('/hangouts')} size="huge" inverted>
+          See what&apos;s going on
+          <Icon name="right arrow" inverted />
+        </Button>
+      </Container>
+    </Segment>
   );
 }
