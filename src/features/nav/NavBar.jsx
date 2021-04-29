a8a368ea85f15df0a4fe1ddb4f260fc147837907
Richard Cho <richardcho100@gmail.com>
2021-04-28 20:40:28 -0400

    Add NavLinks to NavBar menu items

--- src/features/nav/NavBar.jsx
+++ src/features/nav/NavBar.jsx
@@ -1,5 +1,6 @@
 /* eslint-disable react/prop-types */
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 import { Button, Container, Menu } from 'semantic-ui-react';
 
 export default function Navbar({ handleCreateFormDisplayed }) {
@@ -7,13 +8,13 @@ export default function Navbar({ handleCreateFormDisplayed }) {
     <>
       <Menu inverted fixed="top">
         <Container>
-          <Menu.Item header>
+          <Menu.Item as={NavLink} exact to="/" header>
             Hangout
           </Menu.Item>
-          <Menu.Item>
+          <Menu.Item as={NavLink} to="/hangouts">
             Hangouts
           </Menu.Item>
-          <Menu.Item>
+          <Menu.Item as={NavLink} to="/createHangout">
             <Button onClick={handleCreateFormDisplayed} positive inverted content="Create a Hangout" />
           </Menu.Item>
           <Menu.Item position="right">
