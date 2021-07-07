3bcfd8f7d739f2fe7234fb20d4d1ff57374b7743
Richard Cho <richardcho100@gmail.com>
2021-07-06 20:33:27 -0400

    Create test reducer & add to root reducer

--- src/app/store/rootReducer.js
+++ src/app/store/rootReducer.js
@@ -1,10 +1,12 @@
 import { combineReducers } from 'redux';
 import hangoutReducer from '../../features/hangouts/hangoutReducer';
 import modalReducer from '../common/modals/modalReducer';
+import testReducer from '../../features/sandbox/testReducer';
 
 const rootReducer = combineReducers({
   hangoutReducer,
   modalReducer,
+  testReducer,
 });
 
 export default rootReducer;
