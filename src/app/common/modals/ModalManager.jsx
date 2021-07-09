939baf5f468208664c814b2eb221d5b1598830f8
Richard Cho <richardcho100@gmail.com>
2021-07-09 17:28:49 -0400

    Create LoginForm & wrap inside Modal

--- src/app/common/modals/ModalManager.jsx
+++ src/app/common/modals/ModalManager.jsx
@@ -2,10 +2,12 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
 import TestModal from '../../../features/sandbox/TestModal';
+import LoginForm from '../../../features/auth/LoginForm';
 
 export default function ModalManager() {
   const modalCollection = {
     TestModal,
+    LoginForm,
   };
   const currentModal = useSelector((state) => state.modalReducer);
   let renderedModal;
