32c88e19769bcb93be3a338b21216f1a09658998
Richard Cho <richardcho100@gmail.com>
2021-07-12 20:52:40 -0400

    Create separate file for auth constants

--- src/features/auth/authActions.js
+++ src/features/auth/authActions.js
@@ -1,5 +1,4 @@
-const SIGN_IN_USER = 'SIGN_IN_USER';
-const SIGN_OUT_USER = 'SIGN_OUT_USER';
+import { SIGN_IN_USER, SIGN_OUT_USER } from './authConstants';
 
 export function signInUser(payload) {
   return {
