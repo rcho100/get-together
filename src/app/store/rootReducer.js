88b025f4450420926384ccb0843e35d4e4ae8bd5
Richard Cho <richardcho100@gmail.com>
2021-07-12 23:46:23 -0400

    Create authReducer & add to rootReducer

--- src/app/store/rootReducer.js
+++ src/app/store/rootReducer.js
@@ -2,11 +2,13 @@ import { combineReducers } from 'redux';
 import hangoutReducer from '../../features/hangouts/hangoutReducer';
 import modalReducer from '../common/modals/modalReducer';
 import testReducer from '../../features/sandbox/testReducer';
+import authReducer from '../../features/auth/authReducer';
 
 const rootReducer = combineReducers({
   hangoutReducer,
   modalReducer,
   testReducer,
+  authReducer,
 });
 
 export default rootReducer;
